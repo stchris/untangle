9feff700c952486cec05800dd8a514606d3c7d18
Christian Stefanescu <st.chris@gmail.com>
2011-06-05 12:30:12 +0200

    tabsize = 4 refactoring

--- examples.py
+++ examples.py
@@ -4,42 +4,43 @@ import untangle
 
 
 def access():
-	o = untangle.parse('<?xml version="1.0"?><node id="5"><subnode value="abc"/></node>')
-	return "Node id = %s, subnode value = %s" % (o.node['id'], o.node.subnode['value'])
+    o = untangle.parse('<node id="5"><subnode value="abc"/></node>')
+    return "Node id = %s, subnode value = %s" % (o.node['id'], o.node.subnode['value'])
 
 def siblings_list():
-	o = untangle.parse('''<?xml version="1.0"?>
-		<root>
-			<child name="child1"/>
-			<child name="child2"/>
-			<child name="child3"/>
-		</root>
-		''')
-	return ','.join([child['name'] for child in o.root.child])
+    o = untangle.parse('''
+        <root>
+            <child name="child1"/>
+            <child name="child2"/>
+            <child name="child3"/>
+        </root>
+        ''')
+    return ','.join([child['name'] for child in o.root.child])
 
 
 def google_weather():
-	location = raw_input('Select location to retrieve weather data for: ')
-	url = 'http://www.google.com/ig/api?weather=%s' % location
-	o = untangle.parse(url).xml_api_reply
-	s = "Location: %s\n" % o.weather.forecast_information.city['data']
-	s = s +  "Current temperature (Celsius): %s\n" % o.weather.current_conditions.temp_c['data']
-	s = s + "Current temperature (Fahrenheit): %s\n" % o.weather.current_conditions.temp_f['data']
-	s = s + "Forecast:\n"
-	for fc in o.weather.forecast_conditions:
-		s = s + "\t%s: Low %s, High: %s\n" % (fc.day_of_week['data'], fc.low['data'], fc.high['data'])
-	return s
+    location = raw_input('Select location to retrieve weather data for: ')
+    url = 'http://www.google.com/ig/api?weather=%s' % location
+    o = untangle.parse(url).xml_api_reply
+    s = "Location: %s\n" % o.weather.forecast_information.city['data']
+    s = s +  "Current temperature (Celsius): %s\n" % o.weather.current_conditions.temp_c['data']
+    s = s + "Current temperature (Fahrenheit): %s\n" % o.weather.current_conditions.temp_f['data']
+    s = s + "Forecast:\n"
+    for fc in o.weather.forecast_conditions:
+        s = s + "\t%s: Low %s, High: %s\n" % (fc.day_of_week['data'], fc.low['data'], fc.high['data'])
+    return s
 
 examples = [('Access children with parent.children and attributes with element["attribute"]', access),
-			('Access siblings as list', siblings_list),
-			('Fetch the weather from the Google Weather API and show some data', google_weather	)]
+            ('Access siblings as list', siblings_list),
+            ('Fetch the weather from the Google Weather API and show some data', google_weather )]
 
 if __name__ == '__main__':
-	for description, func in examples:
-		print '=' * 70
-		print description
-		print '=' * 70
-		print
-		print func()
-		print
+    for description, func in examples:
+        print '=' * 70
+        print description
+        print '=' * 70
+        print
+        print func()
+        print
 
+# vim: set expandtab ts=4 sw=4:
--- untangle.py
+++ untangle.py
@@ -12,76 +12,77 @@ from xml.sax import make_parser, handler
 from StringIO import StringIO
 
 class Element():
-	def __init__(self, name, attributes):
-		self.name = name
-		self.attributes = attributes
-		self.children = []
+    def __init__(self, name, attributes):
+        self.name = name
+        self.attributes = attributes
+        self.children = []
 
-	def add_child(self, element):
-		element.name = element.name.replace('-', '_')
-		self.children.append(element)
+    def add_child(self, element):
+        element.name = element.name.replace('-', '_')
+        self.children.append(element)
 
-	def get_attribute(self, key):
-		return self.attributes.get(key)
+    def get_attribute(self, key):
+        return self.attributes.get(key)
 
-	def get_elements(self, name=None):
-		if name:
-			return [e for e in self.children if e.name == name]
-		else:
-			return self.children
+    def get_elements(self, name=None):
+        if name:
+            return [e for e in self.children if e.name == name]
+        else:
+            return self.children
 
-	def __getitem__(self, key):
-		return self.get_attribute(key)
+    def __getitem__(self, key):
+        return self.get_attribute(key)
 
-	def __getattr__(self, key):
-		l = [x for x in self.children if x.name == key]
-		if l:
-			if len(l) == 1:
-				self.__dict__[key] = l[0]
-				return l[0]
-			else:
-				self.__dict__[key] = l
-				return l
-		else:
-			raise IndexError('Unknown key <%s>' % key)
+    def __getattr__(self, key):
+        l = [x for x in self.children if x.name == key]
+        if l:
+            if len(l) == 1:
+                self.__dict__[key] = l[0]
+                return l[0]
+            else:
+                self.__dict__[key] = l
+                return l
+        else:
+            raise IndexError('Unknown key <%s>' % key)
 
-	def __str__(self):
-		return "Element <%s> with attributes %s and children %s" % (self.name, self.attributes, self.children)
+    def __str__(self):
+        return "Element <%s> with attributes %s and children %s" % (self.name, self.attributes, self.children)
 
-	def __repr__(self):
-		return "Element(name = %s, attributes = %s)" % (self.name, self.attributes)
+    def __repr__(self):
+        return "Element(name = %s, attributes = %s)" % (self.name, self.attributes)
 
-	def __nonzero__(self):
-		return self.name is not None
+    def __nonzero__(self):
+        return self.name is not None
 
 class Handler(handler.ContentHandler):
-	def __init__(self):
-		self.root = Element(None, None)
-		self.elements = []
+    def __init__(self):
+        self.root = Element(None, None)
+        self.elements = []
 
-	def startElement(self, name, attributes):
-		attrs = dict()
-		for k,v in attributes.items():
-			attrs[k] = v
-		element = Element(name, attrs)
-		if len(self.elements) > 0:
-			self.elements[-1].add_child(element)
-		else:
-			self.root.add_child(element)
-		self.elements.append(element)
+    def startElement(self, name, attributes):
+        attrs = dict()
+        for k,v in attributes.items():
+            attrs[k] = v
+        element = Element(name, attrs)
+        if len(self.elements) > 0:
+            self.elements[-1].add_child(element)
+        else:
+            self.root.add_child(element)
+        self.elements.append(element)
 
-	def endElement(self, name):
-		self.elements.pop()
+    def endElement(self, name):
+        self.elements.pop()
 
 def parse(filename):
-	parser = make_parser()
-	handler = Handler()
-	parser.setContentHandler(handler)
-	try:
-		parser.parse(filename)
-	except IOError as e:
-		# try to see if the passed string is valid XML before giving up
-		parser.parse(StringIO(filename))
+    parser = make_parser()
+    handler = Handler()
+    parser.setContentHandler(handler)
+    try:
+        parser.parse(filename)
+    except IOError as e:
+        # try to see if the passed string is valid XML before giving up
+        parser.parse(StringIO(filename))
 
-	return handler.root
+    return handler.root
 
+# vim: set expandtab ts=4 sw=4:
